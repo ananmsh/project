2a75bf4eb7ea5badcb3d9324a1dd0eafc788b23d
eyalbasis <51421775+eyalbasis@users.noreply.github.com>
2019-06-15 00:25:37 +0300

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,15 +1,73 @@
 package entities;
 
+import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Map {
+import application.MyFile;
+
+public class Map implements Serializable {
 	
+	private String MapName;
 	private String MapDescription;
-	private ArrayList<Place> Locations;
+	private ArrayList<String> Places;
+	private int price;
+	private int NoOfTours;
+	private String CityName;
+	private String version;
+
+	public Map(String name,String description,String CityName)
+	{
+	    this.MapName=name;
+	    MapDescription=description;
+	    Places= new ArrayList<String>();
+	    price=0;
+	    NoOfTours=0;
+	    this.CityName=CityName;
+		this.version= "0";
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
 	
+	public String getCityName() {
+		return CityName;
+	}
+
+	public void setCityName(String cityName) {
+		CityName = cityName;
+	}
+
 	
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getNoOfTours() {
+		return NoOfTours;
+	}
+
+	public void setNoOfTours(int noOfTours) {
+		NoOfTours = noOfTours;
+	}
+
 	
-	
+	public String getMapName() {
+		return MapName;
+	}
+
+	public void setMapName(String mapName) {
+		MapName = mapName;
+	}
 
 	public String getMapDescription() {
 		return MapDescription;
@@ -20,12 +78,12 @@ public class Map {
 	}
 
   
-	public ArrayList<Place> getLocations() {
-		return Locations;
+	public ArrayList<String> getLocations() {
+		return Places;
 	}
 	
-	public void setLocations(ArrayList<Place> locations) {
-		Locations = locations;
+	public void setLocations(ArrayList<String> locations) {
+		Places = locations;
 	}
 	
 	
