b2f48daae55bb60d85d281535e72d6b9ea89c74d
eyalbasis <51421775+eyalbasis@users.noreply.github.com>
2019-06-18 00:05:05 +0300

    Add files via upload

---
+++ CityController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+public class CityController {
+public static String CityName;
+
+public static String getCityName() {
+	return CityName;
+}
+
+public static void setCityName(String cityName) {
+	CityName = cityName;
+} 
+}
